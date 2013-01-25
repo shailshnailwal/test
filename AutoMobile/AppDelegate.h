71d12b3ec48123ee63180ce4f556c5c37fad7272
Shailsh Naiwal <shailsh@Chrome-Mac-17s-Mac-mini.local>
2013-01-25 16:56:52 +0530

    Initial Commit

---
+++ AutoMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoMobile
+//
+//  Created by Shailsh Naiwal on 25/01/13.
+//  Copyright (c) 2013 Shailsh Naiwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
